7cddceef4975192fa9922792f2ed66fb2ce6b2e4
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-08-14 21:27:43 +0000

    generador funcionando

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const theme = ["Block", "Blockchain", "Cripto"];
+  const action = ["Finance", "Exchange", "Tokens"];
+  const host = [".com", ".es", ".io"];
+
+  let result = [];
+
+  theme.forEach(elemento1 => {
+    action.forEach(elemento2 => {
+      host.forEach(elemento3 => {
+        result.push(`${elemento1}${elemento2}${elemento3}`);
+      });
+    });
+  });
+  const random = array => array[Math.floor(Math.random() * array.length)];
+
+  document.getElementById("click").onclick = function() {
+    document.getElementById("domain").innerHTML = random(result);
+  };
 };
